87ba0d24e9de10a7a036388834ef4377e384e245
Samuel Arcanjo <33967635+SamuelArcanjo@users.noreply.github.com>
2018-06-11 23:11:16 -0300

    routes

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,163 @@
+<?php
+
+// Home
+Breadcrumbs::register('home', function ($breadcrumbs) {
+    $breadcrumbs->push('Início', route('home'));
+});
+
+// Product List
+Breadcrumbs::register('products.index', function ($breadcrumbs) {
+    $breadcrumbs->parent('home');
+    $breadcrumbs->push('Produtos', route('products.index'));
+});
+
+// Product Create
+Breadcrumbs::register('products.create', function ($breadcrumbs) {
+    $breadcrumbs->parent('products.index');
+    $breadcrumbs->push('Criar', route('products.create'));
+});
+
+// Product Show
+Breadcrumbs::register('products.show', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('products.index');
+    $breadcrumbs->push('Exibir', route('products.show', $route->parameters));
+});
+
+// Product Edit
+Breadcrumbs::register('products.edit', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('products.index');
+    $breadcrumbs->push('Editar', route('products.edit', $route->parameters));
+});
+
+// Client List
+Breadcrumbs::register('clients.index', function ($breadcrumbs) {
+    $breadcrumbs->parent('home');
+    $breadcrumbs->push('Clientes', route('clients.index'));
+});
+
+// Client Create
+Breadcrumbs::register('clients.create', function ($breadcrumbs) {
+    $breadcrumbs->parent('clients.index');
+    $breadcrumbs->push('Criar', route('clients.create'));
+});
+
+// Client Show
+Breadcrumbs::register('clients.show', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('clients.index');
+    $breadcrumbs->push('Exibir', route('clients.show', $route->parameters));
+});
+
+// Client Edit
+Breadcrumbs::register('clients.edit', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('clients.index');
+    $breadcrumbs->push('Editar', route('clients.edit', $route->parameters));
+});
+
+// Rent List
+Breadcrumbs::register('rents.index', function ($breadcrumbs) {
+    $breadcrumbs->parent('home');
+    $breadcrumbs->push('Aluguéis', route('rents.index'));
+});
+
+// Rent Create
+Breadcrumbs::register('rents.create', function ($breadcrumbs) {
+    $breadcrumbs->parent('rents.index');
+    $breadcrumbs->push('Criar', route('rents.create'));
+});
+
+// Rent Show
+Breadcrumbs::register('rents.show', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('rents.index');
+    $breadcrumbs->push('Exibir', route('rents.show', $route->parameters));
+});
+
+// Rent Edit
+Breadcrumbs::register('rents.edit', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('rents.index');
+    $breadcrumbs->push('Editar', route('rents.edit', $route->parameters));
+});
+
+
+// Profile List
+Breadcrumbs::register('profile', function ($breadcrumbs) {
+    $breadcrumbs->parent('home');
+    $breadcrumbs->push('Perfil', route('profile'));
+});
+
+// Profile Edit
+Breadcrumbs::register('profile.edit', function ($breadcrumbs) {
+    $breadcrumbs->parent('profile');
+    $breadcrumbs->push('Editar', route('profile.edit'));
+});
+
+// Address User Create
+Breadcrumbs::register('address_user.create', function ($breadcrumbs) {
+    $breadcrumbs->parent('profile');
+    $breadcrumbs->push('Criar', route('address_user.create'));
+});
+
+// Address User Show
+Breadcrumbs::register('address_user.show', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('profile');
+    $breadcrumbs->push('Exibir', route('address_user.show', $route->parameters));
+});
+
+// Address User Edit
+Breadcrumbs::register('address_user.edit', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('profile');
+    $breadcrumbs->push('Editar', route('address_user.edit', $route->parameters));
+});
+
+// Phone User Create
+Breadcrumbs::register('phone_user.create', function ($breadcrumbs) {
+    $breadcrumbs->parent('profile');
+    $breadcrumbs->push('Criar', route('phone_user.create'));
+});
+
+// Phone User Show
+Breadcrumbs::register('phone_user.show', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('profile');
+    $breadcrumbs->push('Exibir', route('phone_user.show', $route->parameters));
+});
+
+// Phone User Edit
+Breadcrumbs::register('phone_user.edit', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('profile');
+    $breadcrumbs->push('Editar', route('phone_user.edit', $route->parameters));
+});
+
+// Address Client Create
+Breadcrumbs::register('address_client.create', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('clients.show', $route);
+    $breadcrumbs->push('Criar', route('address_client.create', $route->parameters));
+});
+
+// Address Client Show
+Breadcrumbs::register('address_client.show', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('clients.show', $route);
+    $breadcrumbs->push('Exibir', route('address_client.show', $route->parameters));
+});
+
+// Address Client Edit
+Breadcrumbs::register('address_client.edit', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('clients.show', $route);
+    $breadcrumbs->push('Editar', route('address_client.edit', $route->parameters));
+});
+
+// Phone Client Create
+Breadcrumbs::register('client_phone.create', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('clients.show', $route);
+    $breadcrumbs->push('Criar', route('client_phone.create', $route->parameters));
+});
+
+// Phone Client Show
+Breadcrumbs::register('client_phone.show', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('clients.show', $route);
+    $breadcrumbs->push('Exibir', route('client_phone.show', $route->parameters));
+});
+
+// Phone Client Edit
+Breadcrumbs::register('client_phone.edit', function ($breadcrumbs, $route) {
+    $breadcrumbs->parent('clients.show', $route);
+    $breadcrumbs->push('Editar', route('client_phone.edit', $route->parameters));
+});
---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('/products', 'ProductController');
+    Route::resource('/clients', 'ClientController');
+    Route::resource('/rents', 'RentController');
+
+    Route::get('/profile', 'Auth\ProfileController@index')->name('profile');
+    Route::get('/profile/edit', 'Auth\ProfileController@edit')->name('profile.edit');
+    Route::patch('/profile/update', 'Auth\ProfileController@update')->name('profile.update');
+
+    Route::resource('/address_user', 'AddressUserController', ['except' => ['index']]);
+    Route::resource('/phone_user', 'PhoneUserController', ['except' => ['index']]);
+
+    Route::get('/clients/{client}/addresses/create', 'AddressClientController@create')->name('address_client.create');
+    Route::post('/clients/{client}/addresses/store', 'AddressClientController@store')->name('address_client.store');
+    Route::get('/clients/{client}/addresses/{address}', 'AddressClientController@show')->name('address_client.show');
+    Route::get('/clients/{client}/addresses/{address}/edit', 'AddressClientController@edit')->name('address_client.edit');
+    Route::patch('/clients/{client}/addresses/{address}', 'AddressClientController@update')->name('address_client.update');
+    Route::delete('/clients/{client}/addresses/{address}', 'AddressClientController@destroy')->name('address_client.destroy');
+
+    Route::get('/clients/{client}/phones/create', 'ClientPhoneController@create')->name('client_phone.create');
+    Route::post('/clients/{client}/phones/store', 'ClientPhoneController@store')->name('client_phone.store');
+    Route::get('/clients/{client}/phones/{phone}', 'ClientPhoneController@show')->name('client_phone.show');
+    Route::get('/clients/{client}/phones/{phone}/edit', 'ClientPhoneController@edit')->name('client_phone.edit');
+    Route::patch('/clients/{client}/phones/{phone}', 'ClientPhoneController@update')->name('client_phone.update');
+    Route::delete('/clients/{client}/phones/{phone}', 'ClientPhoneController@destroy')->name('client_phone.destroy');
+});
